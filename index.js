388f1c53faf6cb6d06908a748d2e29a4b7c214fb
Vidisha Srivastav <vidisha.srivastav16@gmail.com>
2018-06-19 13:09:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  string.toLowerCase() === string
+  return "I can't hear you!"
+}
+
+function sayHiToGrandma(string) {
+  var response
+  string.toLowerCase() === string
+  response = "I can't hear you!"
+  
+  string.toUpperCase() === string
+  response =  "YES INDEED!"
+  
+  string === "I love you, Grandma."
+  response = "I love you, too"
+  
+  return response
+}
